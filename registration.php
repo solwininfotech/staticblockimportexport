82aae2ed440e6d87584e1a0ac18c72c31021869c
solwininfotech <info@solwininfotech.com>
2020-05-22 15:54:29 +0530

    initial

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Static Blocks Import Export
+ *
+ * @category   Solwin
+ * @package    Solwin_StaticBlockImportExport
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_StaticBlockImportExport',
+    __DIR__
+);
